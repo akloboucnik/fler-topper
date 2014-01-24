7707b58600053a9760f02b298300f5e4d8437ecb
Adam Kloboucnik <adam.kloboucnik@gooddata.com>
2014-01-24 15:49:11 +0100

    CODE: Added name to uberjar

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :main fler-topper.core
+  :uberjar-name "fler-topper-standalone.jar"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.8"]
                  [enlive "1.1.5"]])
